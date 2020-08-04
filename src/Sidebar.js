1bd43460065b8e84d0632bc0fff9d8947cde34a6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2020-08-04 23:35:03 +0200

    Wider file list

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -34,7 +34,7 @@ const Sidebar = ({ files, selectedFile, selectFile, addFiles, removeFile, remove
                     <tbody>
                         {files.map(f =>
                             <tr key={f.file.name} className={selectedFile === f ? "selected" : null}>
-                                <td onClick={() => selectFile(f)}>
+                                <td onClick={() => selectFile(f)} title={f.file.name}>
                                     <Glyph type={f.isDetailed ? "stats" : "none"} /> {f.file.name}
                                 </td>
                                 <td className="table-col-icon">
